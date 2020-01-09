7336c091ed0b95fab08f343a04b7626b193b5767
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-01-09 16:33:04 +0300

    fix Client[] to Client using as OBJECT (TypeORM)
    Using async Functions

--- src/api/Controller.ts
+++ src/api/Controller.ts
@@ -2,25 +2,33 @@ import { Router } from 'express';
 import { Request } from 'express';
 import { Response } from 'express';
 import { NextFunction } from 'express';
+import router from './routerApi';
 export abstract class Controller{
     mainRouter: Router = Router()
-    abstract createConnectionAndAssignRepository() : any;
-    abstract addAllRoutes(router : Router):void;
-    abstract addGet(router : Router):void;
-    abstract addPost(router : Router):void;
-    abstract addDelete(router : Router):void;
-    abstract addPut(router : Router):void;
-    addErrorHandler(router: Router) {
+    abstract async createConnectionAndAssignRepository() : Promise<any>;
+    protected async addAllRoutes(router : Router):Promise<void>{
+        await this.addGet(router)
+        await this.addPost(router)
+        await this.addDelete(router)
+        await this.addPut(router)
+        await this.addErrorHandler(router)
+    }
+
+    abstract async addGet(router : Router):Promise<void>;
+    abstract async addPost(router : Router):Promise<void>;
+    abstract async addDelete(router : Router):Promise<void>;
+    abstract async addPut(router : Router):Promise<void>;
+    protected async addErrorHandler(router: Router) {
         router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
             console.log(err)
             this.sendResponse(res, 500, { error: err })
         })
     }
 
-    sendResponse(response: Response, statusCode: number, data: Object) {
+    protected async sendResponse(response: Response, statusCode: number, data: Object) {
         response.status(statusCode).json(data)
     }
-    passErrorToExpress(err: Error, next: NextFunction) {
+    protected async passErrorToExpress(err: Error, next: NextFunction) {
         next(err)
     }
 }
