b516f874ce03657261622b5ca19b1843594b18a6
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-01-20 17:21:35 +0300

    Adding materiel to route

--- src/api/routerApi.ts
+++ src/api/routerApi.ts
@@ -2,8 +2,10 @@ import express from "express"
 import ClientController from './routes/ClientController';
 import ReservationController from './routes/ReservationController';
 import SalleController from './routes/SalleController';
+import MaterielController from './routes/MaterielController';
 var router = express.Router()
 router.use("/clients", new ClientController().mainRouter);
 router.use("/reservations", new ReservationController().mainRouter);
 router.use("/salles", new SalleController().mainRouter);
+router.use("/materiels", new MaterielController().mainRouter);
 export default router;
