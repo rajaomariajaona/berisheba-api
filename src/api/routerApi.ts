296eb83c3ffb77adf5ce6b0dd014ce536014a693
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-02-04 17:17:40 +0300

    Add jirama to router

--- src/api/routerApi.ts
+++ src/api/routerApi.ts
@@ -4,10 +4,12 @@ import ReservationController from './routes/ReservationController';
 import SalleController from './routes/SalleController';
 import MaterielController from './routes/MaterielController';
 import ConstituerController from './routes/ConstituerController';
+import JiramaController from './routes/JiramaController';
 var router = express.Router()
 router.use("/clients", new ClientController().mainRouter);
 router.use("/reservations", new ReservationController().mainRouter);
 router.use("/salles", new SalleController().mainRouter);
 router.use("/materiels", new MaterielController().mainRouter);
 router.use("/constituers", new ConstituerController().mainRouter);
+router.use("/jirama", new JiramaController().mainRouter);
 export default router;
