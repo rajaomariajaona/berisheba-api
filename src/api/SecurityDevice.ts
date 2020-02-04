1e2c76bb63491557312baed36e2172d4ac551ad7
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-02-04 13:00:13 +0300

    jwt send error when jwt token incorrect

--- src/api/SecurityDevice.ts
+++ src/api/SecurityDevice.ts
@@ -1,21 +1,47 @@
 
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-export default function securityDevice(req: Request, res: Response, next: NextFunction){
+import { createConnection, getConnection, Connection } from 'typeorm';
+import { ormconfig } from '../config';
+import { Device } from '../entities/Device';
+export default function securityDevice(req: Request, res: Response, next: NextFunction) {
     var jwtToken: string = req.headers["authorization"]
-    if(jwtToken == undefined){
+    console.log(jwtToken)
+    if (jwtToken == undefined) {
         res.status(401).send({
-            message: "JWT token absent"
+            message: "JWT token Missing"
         })
         return;
     }
     try {
-        jwt.verify(jwtToken.split(" ")[1], process.env.AUTHORIZATION_PASS_PHRASE)
+        jwt.verify(jwtToken.split(" ")[1], process.env.AUTHORIZATION_PASS_PHRASE, (error, payload) => {
+            if (error == null) {
+                var connection: Connection = getConnection()
+                connection.getRepository(Device).findOneOrFail(payload.deviceid)
+                    .then((device) => {
+                        if (device.authorized) {
+                            next()
+                            return;
+                        } else {
+                            res.status(401).send({
+                                message: "device not authorized"
+                            })
+                        }
+                    }).catch(error => {
+                        res.status(401).send({
+                            message: "device not authorized"
+                        })
+                    })
+            }else{
+                res.status(401).send({
+                    message: error.message
+                })
+            }
+        })
     } catch (error) {
         res.status(401).send({
             message: error.message
         })
         return;
     }
-    next()
 }
