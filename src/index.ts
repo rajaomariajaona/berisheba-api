a3ae838ef41545ce3b91283fd27bffe82076de82
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-03-07 21:31:25 +0300

    Adding default User for admin

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import 'reflect-metadata';
 import RunServer from './api/RunServer';
 import {MyWebSocket} from "./websocket/ws";
 import dotenv from 'dotenv'
+import { createConnection } from 'typeorm';
+import { ormconfig } from './config';
+import { User } from './entities/User';
 dotenv.config()
 const server = RunServer();
 const WebSocket = new MyWebSocket(server);
+createConnection(ormconfig).then( async (connection) => {
+    let repos = connection.getRepository(User);
+    await repos.count().then(async (count) => {
+        if(count <= 0){
+            var defaultUser = new User();
+            defaultUser.username = "admin"
+            defaultUser.email = "mamyseheno1@gmail.com"
+            defaultUser.password = "$2b$10$VxrrwC4cmwaO1OZ6dk72KeTOh8s2gx7m8BCzHmh.NjsWt7D2wxDxq"
+            await repos.save(defaultUser)
+        }
+    })
+})
